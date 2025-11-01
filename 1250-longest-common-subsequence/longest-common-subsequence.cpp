f868ff86ffeb78825a3178c5f5838c0381a38962
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-02 00:08:44 +0530

    Time: 11 ms (97.89%) | Memory: 9 MB (99.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,31 +2,51 @@ class Solution {
 public:
     int find(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
         if (i < 0 || j < 0)
-            return dp[i+1][j+1]=0;
-        if (dp[i+1][j+1] != -1)
-            return dp[i+1][j+1];
+        
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         if (s1[i] == s2[j])
-            return  dp[i+1][j+1]=1 + find(i - 1, j - 1, s1, s2, dp);
+            return  dp[i][j]=1 + find(i - 1, j - 1, s1, s2, dp);
 
-        return dp[i+1][j+1] =
+        return dp[i][j] =
                    max(find(i, j - 1, s1, s2, dp), find(i - 1, j, s1, s2, dp));
     }
-    int longestCommonSubsequence(string s1, string s2) {
+    int longestCommonSubsequenceTABULATION(string s1, string s2) {
         int n = s1.size();
         int m = s2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
 
-        //         if (s1[i] == s2[j])
-        //             dp[i][j] = 1 + dp[i - 1][j - 1];
+                if (s1[i] == s2[j])
+                    dp[i+1][j+1] = 1 + dp[i][j ];
+                else    
+                dp[i+1][j+1] = max(dp[i+1][j ], dp[i ][j+1]);
+            }
+        }
+       return dp[n][m];
 
-        //         dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-        //     }
-        // }
-       return find(n-1,m-1,s1,s2,dp);
+    }
+
+        int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+         vector<int>  curr(m+1, 0);
+         vector<int>  prev(m+1, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (s1[i] == s2[j])
+                    curr[j+1] = 1 + prev[j];
+                else    
+                curr[j+1] = max(curr[j ], prev[j+1]);
+            }
+            prev=curr;
+        }
+       return prev[m];
 
     }
 };
