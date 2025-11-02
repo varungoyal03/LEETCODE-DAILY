05f02cece548d906b328183dd56d812b3c4d2238
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-02 14:40:30 +0530

    Time: 4 ms (58.80%) | Memory: 10 MB (43.56%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n1=s1.size();
+        int n2=s2.size();
+        if(n1>n2) return false;
+
+        vector<int> freq(26,0);
+        for(char &ch:s1){ freq[ch-'a']++;}
+
+        vector<int> freq1(26, 0), freq2(26, 0);
+//precoess first window
+        for (char c : s1) freq1[c - 'a']++;
+        for (int i = 0; i < n1; i++) freq2[s2[i] - 'a']++;
+
+        if (freq1 == freq2) return true;
+//slide
+        for (int i = n1; i < n2; i++) {
+            freq2[s2[i] - 'a']++;              // add new char
+            freq2[s2[i - n1] - 'a']--;         // remove old char
+
+            
+            if (freq1 == freq2) return true;   // check match
+        }
+
+        return false;
+
+        
+    }
+};
