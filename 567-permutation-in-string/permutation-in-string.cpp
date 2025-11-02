74f77ea94597ab1055ea51811889e6aa1a57c26d
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-02 15:02:19 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (67.15%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -12,16 +12,31 @@ public:
 //precoess first window
         for (char c : s1) freq1[c - 'a']++;
         for (int i = 0; i < n1; i++) freq2[s2[i] - 'a']++;
-
-        if (freq1 == freq2) return true;
-//slide
-        for (int i = n1; i < n2; i++) {
-            freq2[s2[i] - 'a']++;              // add new char
-            freq2[s2[i - n1] - 'a']--;         // remove old char
-
-            
-            if (freq1 == freq2) return true;   // check match
+int matches=0;
+        for(int i=0;i<26;i++){
+            if(freq1[i]==freq2[i]) matches++;
         }
+        if(matches==26) return true;
+//slide
+
+        for (int i = n1; i < n2; i++) {
+            int newCh=s2[i] - 'a';
+            int oldCh=s2[i - n1] - 'a';
+
+            freq2[newCh]++;              // add new char
+        
+
+
+            if (freq1[newCh] == freq2[newCh]) matches++; 
+            else if (freq1[newCh]+1 == freq2[newCh]) { matches--;}
+
+  freq2[oldCh]--;         // remove old char
+            if (freq1[oldCh] == freq2[oldCh]) matches++; 
+            else if (freq1[oldCh]-1 == freq2[oldCh]) { matches--;}
+
+            if(matches==26) return true;
+        }
+
 
         return false;
 
