7217f1c90ba7fccb663dc03c4876b1e4569cb8f4
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-04 15:22:34 +0530

    Time: 23 ms (85.10%) | Memory: 15.2 MB (90.29%) - LeetSync

--- 1517-restore-the-array/restore-the-array.cpp
+++ 1517-restore-the-array/restore-the-array.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-int MOD=1e9+7;
+    // long long and  if (s[i] == '0') aryan
+    int MOD = 1e9 + 7;
     vector<int> dp;
-    int solve(int i, string& s, int& k) {
+    int solveMEMO(int i, string& s, int& k) {
         if (i == s.size())
             return dp[i] = 1;
 
@@ -12,20 +13,47 @@ int MOD=1e9+7;
         if (dp[i] != -1)
             return dp[i];
 
-        long long ans = 0 ; 
-        long long  num = 0;//
+        long long ans = 0;
+        long long num = 0; //
         for (int start = i; start < s.size(); start++) {
             num = num * 10 + s[start] - '0';
 
             if (num > k)
                 break;
-            ans = (ans%MOD +solve(start + 1, s, k)%MOD  )%MOD;
+            ans = (ans % MOD + solveMEMO(start + 1, s, k) % MOD) % MOD;
         }
 
         return dp[i] = ans;
     }
     int numberOfArrays(string s, int k) {
-        dp.assign(s.size() + 1, -1);
-        return solve(0, s, k);
+        int n = s.size();
+        dp.assign(n + 1, -1);
+        // return solveMEMO(0, s, k);
+
+        dp[n] = 1;
+
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (s[i] == '0') {
+                dp[i] = 0;
+                continue;
+            }
+            long long ans = 0;
+            long long num = 0; //
+            for (int start = i; start < n; start++) {
+                num = num * 10 + s[start] - '0';
+
+                if (num > k)
+                    break;
+
+                ans = (ans % MOD + dp[start + 1] % MOD) % MOD;
+                
+            }
+            dp[i] = ans;//forgot
+        }
+
+
+        return dp[0];
     }
 };
