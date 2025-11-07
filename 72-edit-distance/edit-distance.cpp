6eb2f57eed4fb43808c5786927e66f1e1624d9a2
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-07 17:41:25 +0530

    Time: 9 ms (39.17%) | Memory: 13.2 MB (37.23%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -21,11 +21,48 @@ public:
         }
 return dp[i][j]= ans;
     }
-    int minDistance(string s1, string s2) {
-        dp.assign(s1.size(), vector<int>(s2.size(),-1));
 
-      return  solve(s1,s2,0,0);
-    }
+
+    int minDistance(string s1, string s2) {
+        dp.assign(s1.size()+1, vector<int>(s2.size()+1,-1));
+
+
+    //   return  solve(s1,s2,0,0);
+    for(int j=0;j<=s2.length();j++ ){
+            dp[s1.size()][j]=s2.size()-j;
+        }
+
+           for(int i=0;i<=s1.length();i++ ){
+            dp[i][s2.size()]=s1.size()-i;
+        }
+
+
+     
+
+        for(int i=s1.length()-1;i>=0;i--){
+            for(int j=s2.length()-1;j>=0;j--){
+                    int ans=0;
+                    if(s1[i]==s2[j]) ans=dp[i+1][j+1];
+
+                    else{//I D R
+                        ans=1+ min({ dp[i][j+1], 
+                                        dp[i+1][j], 
+                                        dp[i+1][j+1]
+                        });
+                            
+                    }
+                   dp[i][j]= ans;
+
+            }}
+        
+
+
+
+
+
+
+
+  return dp[0][0];  }
 
 
 };
