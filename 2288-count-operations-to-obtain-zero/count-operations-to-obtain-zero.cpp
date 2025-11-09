2a1203db6d671a6d17cbc995f15dacfbc30eb643
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-09 22:36:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (6.11%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+
+        int count=0;
+        while (num1 && num2){
+            if(num1>=num2){
+                count+=num1/num2;
+                num1%=num2;
+
+            }
+            else{
+                count+=num2/num1;
+                num2%=num1;
+            }
+        }
+        
+    return count;}
+};
