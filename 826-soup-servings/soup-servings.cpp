c1bec821bfe9c06358d615a5ac44032829cc9e37
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-09 11:13:33 +0530

    Time: 117 ms (35.47%) | Memory: 428 MB (12.16%) - LeetSync

---
+++ 826-soup-servings/soup-servings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+     vector<vector<int>> serves={{100, 0}, {75, 25}, {50, 50}, {25, 75}};
+
+    vector<vector<double>> t;
+     double solve(int A,int B){
+         if(A <= 0 && B <= 0)
+            return 0.5;
+        
+        if(A <= 0)
+            return 1.0;
+        if(B <= 0)
+            return 0.0;
+
+        if(t[A][B] != -1.0)
+            return t[A][B];
+
+
+            double totalProb=0;
+        for(auto &serve:serves){
+               totalProb+=0.25*solve(A-serve[0],B-serve[1]);
+        }
+
+    return t[A][B]=totalProb;
+     }
+    double soupServings(int n) {
+        if(n>=5000) return 1;
+        t.resize(n+1,vector<double> (n+1,-1.0));
+        return solve(n,n);
+    }
+};
