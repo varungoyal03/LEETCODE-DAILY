cbb45a21a7bc9f5562640b3edb3492e76a7cdaa5
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-10 14:23:08 +0530

    Time: 170 ms (18.36%) | Memory: 13.9 MB (32.31%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+vector<int> t;
+    int solve(int n){
+        if(n==0 ) return 0;
+        if(t[n]!=-1) return t[n];
+
+            int ans=INT_MAX;
+        for(int i=1;i*i<=n;i++){
+           ans=min(ans,1+ solve(n-i*i));
+        }
+
+        return t[n]=ans;
+    }
+    int numSquares(int n) {
+        t.assign(n+1,-1);
+
+
+        // return solve(n);
+
+        t[0]=0;
+
+        for(int i=1;i<=n;i++){
+            
+            int ans=INT_MAX;
+        for(int i=1;i*i<=n;i++){
+           ans=min(ans,1+ solve(n-i*i));
+        }
+
+         t[n]=ans;
+        }
+   return t[n]; }
+};
