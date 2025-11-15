75cc9bf33643cdc1e960597f9aad188525b4c8bc
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-15 16:08:39 +0530

    Time: 15 ms (73.12%) | Memory: 34.8 MB (66.62%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+typedef pair<int,int> PII;
+vector<pair<int, int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+
+        queue<PII> q;
+
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){ q.emplace(i,j);
+                dist[i][j]=0;}
+            }
+        }
+
+int level=0;
+
+        while(!q.empty()){
+          
+            
+
+            int size=q.size();
+            while(size-- ){
+
+                auto [r,c]=q.front(); q.pop();
+
+                for(auto [dx,dy]:dirs){
+                int x=r+dx; int y=c+dy;
+
+                if(x<0 || y<0 || x>=m || y>=n ) continue;
+
+                if(dist[x][y]> level+1 ) {
+                    dist[x][y]=level+1;//
+                    //visted cells will have distance less than level
+                    q.emplace(x,y);
+                }  }
+         
+         
+         
+
+            }
+            level++;
+
+        }
+
+
+
+
+return dist;
+
+    }
+};
