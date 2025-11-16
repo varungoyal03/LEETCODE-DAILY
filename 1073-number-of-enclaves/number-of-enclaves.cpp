5859423bd2077e1741f09944508db9d74acfa0cb
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-16 16:50:13 +0530

    Time: 6 ms (81.35%) | Memory: 34.1 MB (52.00%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+void dfs(vector<vector<int>>& grid,int i,int j){
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != 1) {
+            return;
+        }
+        grid[i][j] = 0; // mark as visited
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+
+}
+    int numEnclaves(vector<vector<int>>& grid) {
+        //If from a land cell you can eventually reach outside the grid, that land cell is NOT counted.
+
+                int m = grid.size();
+        int n = grid[0].size();
+        
+        // mark all boundary land cells and their connected cells
+        for (int i = 0; i < m; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, n-1);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(grid, 0, j);
+            dfs(grid, m-1, j);
+        }
+        
+        // count unmarked land cells
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    
+    
+    }
+};
