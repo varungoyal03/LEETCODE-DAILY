c0917ca01cc4836bb4fc01cf3aea75b2e89ae73c
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2025-11-18 22:01:27 +0530

    Time: 3 ms (92.82%) | Memory: 10 MB (88.30%) - LeetSync

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     vector<int> countOfPairs(int n, int x, int y) {
-        vector<vector<int>> dp(n+1,vector<int>(n+1,INT_MAX));
-        
-         for(int i=1;i<=n;i++) dp[i][i]=0;
 
-        for(int i=1;i<n;i++){
-            
-            dp[i][i+1]=1;
-            dp[i+1][i]=1; //
-        }
-        if(x!=y){
-        dp[x][y]=1;
-        dp[y][x]=1;}
-
-        for(int k=1;k<=n;k++){
-            for(int i=1;i<=n;i++){
-                if(dp[i][k]==INT_MAX) continue;
-                for(int j=1;j<=n;j++){
-                        if(dp[k][j]==INT_MAX) continue;
-
-                    dp[i][j]=min(dp[i][j],dp[i][k]+dp[k][j]);
-                }
-            }
-        }
 
 
          vector<int> result(n);
         for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
+            for(int j = i+1; j <= n; j++) {
 
-                if(i != j) {
-                    int val = dp[i][j];
-                    result[val-1]++;
-                }
+               
+                    
+        
+                int direct = j - i;
+
+                int viaXY = abs(i - x) + 1 + abs(j - y);
+                int viaYX = abs(i - y) + 1 + abs(j - x);
+
+                int d = min({ direct, viaXY, viaYX });
+
+                    result[d-1]+=2;
+                
 
             }
         }
