d1594e3da0ef161c2d7d5a42f9641e2de9f97abe
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-02-03 00:33:37 +0530

    Time: 59 ms (35.00%) | Memory: 149.7 MB (5.00%) - LeetSync

--- 4118-design-ride-sharing-system/design-ride-sharing-system.cpp
+++ 4118-design-ride-sharing-system/design-ride-sharing-system.cpp
@@ -1,48 +1,51 @@
-class RideSharingSystem {
-public:
 
-    // vector<bool> availrider(1001,false);
-      bool availrider[1001]={};
-    queue<int> riders, drivers;
-    RideSharingSystem() {
-        
-    }
+
+class RideSharingSystem {
+private:
+    list<int> ridersList;
+    list<int> driversList;
+
+    unordered_map<int, list<int>::iterator> riderPos;
     
+
+public:
+    RideSharingSystem() {}
+
     void addRider(int riderId) {
-        riders.push(riderId);
-        availrider[riderId] = true;
-    }
+      
+        ridersList.push_back(riderId);
     
+        riderPos[riderId] = --ridersList.end();;
+    }
+
     void addDriver(int driverId) {
-        drivers.push(driverId);
+       
+        driversList.push_back(driverId);
+      
     }
-    
+
     vector<int> matchDriverWithRider() {
-        vector<int> ans = {-1,-1};
-        while(riders.size() && drivers.size()) {
-            int rider = riders.front();
-            riders.pop();
-            if(availrider[rider]) {
-                ans[1] = rider;
-                ans[0] = drivers.front();
-                availrider[rider]=false;
-                drivers.pop();
-                break;
-            }
+        if (driversList.empty() || ridersList.empty()) {
+            return {-1, -1};
         }
-        return ans;
+
+        int driverId = driversList.front();
+        int riderId  = ridersList.front();
+
+        driversList.pop_front();
+        ridersList.pop_front();
+
+     
+        riderPos.erase(riderId);
+
+        return {driverId, riderId};
     }
-    
+
     void cancelRider(int riderId) {
-        availrider[riderId] = false;
+        if (!riderPos.count(riderId)) return;
+
+        auto it = riderPos[riderId];
+        ridersList.erase(it);
+        riderPos.erase(riderId);
     }
 };
-
-/**
- * Your RideSharingSystem object will be instantiated and called as such:
- * RideSharingSystem* obj = new RideSharingSystem();
- * obj->addRider(riderId);
- * obj->addDriver(driverId);
- * vector<int> param_3 = obj->matchDriverWithRider();
- * obj->cancelRider(riderId);
- */
