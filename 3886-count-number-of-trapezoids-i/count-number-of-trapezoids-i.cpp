ca9fa42f245408cb224deea985f07e8a45d476e2
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-02-06 10:54:59 +0530

    Time: 173 ms (20.31%) | Memory: 225.2 MB (24.22%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -1,27 +1,34 @@
+
+using ll = long long;
+const int M = 1e9 +7;
 class Solution {
-    using ll=long long;
 public:
-
-    const int M = 1e9+7;
     int countTrapezoids(vector<vector<int>>& points) {
-        ll ans =0;
 
-        map<int,int> mp;
+    
+
+       unordered_map<int,int> mp;
         for(auto i: points){
             mp[i[1]]++;
         }
-
         ll edgesSum =0;
-//FOR EACH TOP SEE NO, OF BOTTOM 
+        ll cnt_of_same_line=0;
+
         for(auto i: mp ){
-
+ 
             ll edges = (i.second*1LL*(i.second-1))/2;
+            cnt_of_same_line += edges*(edges-1)/2;
 
-            ans = (ans + edges*edgesSum)% M;
+            
             edgesSum+=edges;
         }
 
+        edgesSum%=M;
+        ll total_ways = edgesSum*(edgesSum-1)/2; 
+        total_ways%=M;
+        cnt_of_same_line%=M;
+
+        ll ans=(total_ways-cnt_of_same_line+M)%M;
         return ans;
     }
 };
-
