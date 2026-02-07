4d73863a7082c7c43dabed73913a563a804aa217
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-02-07 17:48:41 +0530

    Time: 1424 ms (5.01%) | Memory: 454.1 MB (5.86%) - LeetSync

---
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& blds) {
+        map<int, set<int>> mapx;
+        map<int, set<int>> mapy;
+
+        for (auto& v : blds) {
+            mapx[v[0]].insert(v[1]);
+            mapy[v[1]].insert(v[0]);
+        }
+
+        int ans = 0;
+        for (auto& v : blds) {
+            // int xsize = mapx[v[0]].size();
+            // int ysize = mapy[v[1]].size();
+            
+            // if (xsize <= 2 || ysize <= 2) continue;
+            if (*mapx[v[0]].begin() == v[1] || *mapx[v[0]].rbegin() == v[1]) continue;
+            if (*mapy[v[1]].begin() == v[0] || *mapy[v[1]].rbegin() == v[0]) continue;
+
+            ans++; 
+        }
+
+        return ans;
+    }
+};
