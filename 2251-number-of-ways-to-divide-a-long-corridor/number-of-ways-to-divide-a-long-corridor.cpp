ebcc9fe51707d9751838f23d393f18c25596737e
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-02-08 13:03:16 +0530

    Time: 37 ms (23.24%) | Memory: 58.7 MB (30.04%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
+    const int mod = 1e9 + 7;
     int numberOfWays(string corridor) {
-        int n=corridor.size();
-              const int M = 1e9 + 7;
-        int totalSeats = 0;
-        for (char c : corridor) {
-            if (c == 'S') totalSeats++;
+        vector<int> pos;
+        for (int i = 0; i < corridor.size(); i++) {
+            if (corridor[i] == 'S') {
+                pos.push_back(i);
+            }
         }
-        if (totalSeats == 0 || totalSeats % 2 != 0)
+        
+        if (pos.size() % 2 or pos.size() == 0) {
             return 0;
-
-
-          long long ways = 1;
-        int seatCount = 0;
-        int prev = -1;
-
-        for(int i=0;i<n;i++){
-            if(corridor[i]=='P') continue;
-
-            seatCount++;
-
-            if(seatCount==2){
-                 prev=i; 
-                seatCount=0;
-              
-            }
-
-            else if (seatCount==1 && prev!=-1){
-                ways*= (i-prev);
-                ways%=M;
-
-            }
+        }
+        
+        long res = 1;
+        for (int i = 2; i < pos.size(); i += 2) {
+            int len_of_gap = pos[i] - pos[i - 1];
+            res = (res * len_of_gap) % mod;
         }
 
-        return ways;
+        return res;
     }
 };
