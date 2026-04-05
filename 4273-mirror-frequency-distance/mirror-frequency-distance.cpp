4714113c50768d4185948a1b73456df86aff3c93
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-04-05 15:47:15 +0530

    Time: 17 ms (94.26%) | Memory: 58.1 MB (82.28%) - LeetSync

---
+++ 4273-mirror-frequency-distance/mirror-frequency-distance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mirrorFrequency(string s) {
+        vector<int> f1(10), f2(26);
+
+        for (char &ch: s) {
+            if (ch <= '9' && ch >= '0') {
+                f1[ch-'0']++;
+            } else {
+                f2[ch-'a']++;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 10; i++) {
+            ans += abs(f1[i] - f1[9 - i]);
+        }
+
+        for (int i = 0; i < 26; i++) {
+            ans += abs(f2[i] - f2[25 - i]);
+        }
+
+        return ans/2;
+    }
+};
+
+//s 
+// mirror ch
+//f(ch)-f(m:ch)
+
