bd98a8898912c7a789071fb7862ccae88aa28ef1
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-04-09 14:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (94.14%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-vector<int> time={1,7,30};
- vector<int> dp;
-
-    int solve(int i,vector<int>& days, vector<int>& costs){
-        int n=days.size();
-        if(i==days.size()) return 0;
-        if(dp[i] != -1) return dp[i];
-
-        int cost1 = costs[0] + solve(i + 1, days, costs);
-
-         int j = i;
-        while (j < n && days[j] <= days[i] + 6) j++;
-        int cost7 = costs[1] + solve(j, days, costs);
-
-        int k = i;
-        while (k < n && days[k] <= days[i] + 29) k++;
-        int cost30 = costs[2] + solve(k, days, costs);
-
-
-           return dp[i] = min({cost1, cost7, cost30});
-
-
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int n=days.size();
-       dp.assign(n+1,-1);
-        return solve(0,days,costs);
-        
+        vector<bool> travel(366, false);
+        for (int d : days) travel[d] = true;
+
+        vector<int> dp(367, 0);
+
+        for (int day = 365; day >=0; day--) {
+            if (!travel[day]) {
+                dp[day] = dp[day +1];
+            } else {
+
+                dp[day] = min({
+                    dp[day + 1] + costs[0],
+                    dp[min(366, day +7)] + costs[1],
+                    dp[min(366, day + 30)] + costs[2]
+                });
+            }
+        }
+
+        return dp[1];
     }
 };
