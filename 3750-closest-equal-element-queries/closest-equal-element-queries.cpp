80b61aaa982caa03f46173f1cf73d441b5b6dd1b
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-04-16 16:32:35 +0530

    Time: 1007 ms (5.47%) | Memory: 309.1 MB (5.47%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
         int n=nums.size();
-//direct simulating 3*n we need to handle edge cases
-
-        for(int i=0;i<n;i++) nums.push_back(nums[i]);
-        for(int i=0;i<n;i++) nums.push_back(nums[i]);
-
+//direcy simulating 3*n we need to handle edge cases
         set<pair<int,int>> s;
-        for(int i=0;i<3*n;i++){
+        for(int i=0;i<n;i++){
             s.insert({nums[i],i});
+            s.insert({nums[i],i+n});
+            s.insert({nums[i],i+2*n});
         }
         vector<int> ans;
 
         for(auto &q:queries){
-          
-                q+=n;//
-                
-            auto it=s.lower_bound({nums[q],q});
-            int rightDist=next(it)->second-q;
-            int leftDist=q-prev(it)->second;
+          int p=q+n;
+
+            auto it=s.lower_bound({nums[q],p});//
+            int rightDist=next(it)->second-p;
+            int leftDist=p-prev(it)->second;
 
            int result= min({leftDist,rightDist});
            ans.push_back(result==n?-1:result);
