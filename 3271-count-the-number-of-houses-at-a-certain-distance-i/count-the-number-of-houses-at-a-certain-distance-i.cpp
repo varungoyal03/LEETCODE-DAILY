462c7491511e76d249ce7ee41428f7635c69af19
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-06-04 00:52:22 +0530

    Time: 401 ms (23.20%) | Memory: 19.1 MB (69.59%) - LeetSync

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     vector<int> countOfPairs(int n, int x, int y) {
-
-
-
-         vector<int> result(n);
-        for(int i = 1; i <= n; i++) {
-            for(int j = i+1; j <= n; j++) {
-
-               
-                    
+        vector<vector<int>> grid(n+1, vector<int>(n+1, 1e9));
         
-                int direct = j - i;
+        for(int j = 1; j < n; j++) {
+            grid[j][j+1] = 1;
+            grid[j+1][j] = 1;
+        }
+        grid[x][y] = 1;
+        grid[y][x] = 1;
+        
+        for(int k = 1; k <= n; k++) {
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) {              
+                   grid[i][j] = min(grid[i][j], grid[i][k] + grid[k][j]);                
+                }
+            }
+        }
+        
+        vector<int> result(n);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
 
-                int viaXY = abs(i - x) + 1 + abs(j - y);
-                int viaYX = abs(i - y) + 1 + abs(j - x);
-
-                int d = min({ direct, viaXY, viaYX });
-
-                    result[d-1]+=2;
-                
+                if(i != j) {
+                    int val = grid[i][j];
+                    result[val-1]++;
+                }
 
             }
         }
         
         return result;
-
-
-
+        
     }
 };
