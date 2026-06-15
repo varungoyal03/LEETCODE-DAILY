3d4eada131d0fec39cfe60d5bff1c7de898f2691
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-06-15 18:39:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (17.26%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -3,8 +3,7 @@ public:
 vector<int> dp;
     int solve(int n){
         if(n==0) return 1;  // IMP
-        if(n==1) return 1;
-        if(n==2) return 2;
+        
         
 
         if(dp[n]!=-1) return dp[n];
