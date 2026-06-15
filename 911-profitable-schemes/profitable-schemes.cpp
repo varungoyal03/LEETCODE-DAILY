0cdd49bbf55e3827ce089b810b386a56e86ed30a
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-06-15 14:05:29 +0530

    Time: 84 ms (66.07%) | Memory: 15.5 MB (64.27%) - LeetSync

--- 911-profitable-schemes/profitable-schemes.cpp
+++ 911-profitable-schemes/profitable-schemes.cpp
@@ -11,7 +11,7 @@ public:
         if(  t[i][n][minProfit]!=-1 ) return   t[i][n][minProfit];
         long long notTake=solve(i+1,n,minProfit,group,profit);
         long long take=0;
-        if( n!=0 && n-group[i]>=0 ){
+        if(  n-group[i]>=0 ){
             int newProfit=max(minProfit-profit[i] ,0); 
             take=solve(i+1,n-group[i], newProfit,group,profit);}
 
@@ -21,44 +21,14 @@ public:
   
     int profitableSchemes(int N, int minprofit, vector<int>& group, vector<int>& profit) {
         // ALL POSSIIIBEL SUBSET EXPOLE==> TAKE IT OR NOT TAKE
-//  memset(t,-1,sizeof(t));
-//        return  solve(0,N,minprofit,group,profit);
+ memset(t,-1,sizeof(t));
+       return  solve(0,N,minprofit,group,profit);
 
 
 
 
    
     
-        memset(t,0,sizeof(t));
+}
 
-int m = group.size();
-for (int g = 0; g <= N; ++g)
-    t[m][g][0] = 1;   // base case: if we've considered all crimes, 1 way to achieve profit 0
-
-
-
-        for(int i=group.size()-1;i>=0;i--){
-            for(int n=0;n<=N;n++){
-                for(int minProfit=0;minProfit<=minprofit;minProfit++){
-                    
-        long long notTake=t[i+1][n][minProfit];
-        long long take=0;
-        if( n!=0 && n-group[i]>=0 ){
-            int newProfit=max(minProfit-profit[i] ,0);
-            take=t[i+1][n-group[i]][newProfit];}
-
-  t[i][n][minProfit]= (take+notTake)%MOD;
-
-
-                }
-
-            }
-        }
-    return t[0][N][minprofit]; }
 };
-
-
-
-
-
-
