5005834294ae61b00ba535edb3c4f3831239ba14
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-06-26 11:13:06 +0530

    Time: 43 ms (6.58%) | Memory: 16.2 MB (58.70%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,38 +1,27 @@
 class Solution {
-    bool isVowel(char c) {
-        return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
-               c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
 public:
     string sortVowels(string s) {
-        vector<int> count(256, 0);
-        
-        // Count every vowel in the string
+        vector<char> vowels;
         for (char c : s) {
             if (isVowel(c)) {
-                count[c]++;
+                vowels.push_back(c);
             }
         }
-        
-        // The perfectly sorted order of vowels based on ASCII values
-        string sortedVowels = "AEIOUaeiou";
-        int vowel_idx = 0; // Pointer to iterate through our 10 buckets
-        
-        // Step 2: Replace vowels in the original string
-        for (int i = 0; i < s.length(); i++) {
+
+        sort(vowels.begin(), vowels.end());
+
+        int v_index = 0;
+        for (int i = 0; i < s.size(); i++) {
             if (isVowel(s[i])) {
-                
-                // Skip empty buckets until we find a vowel we actually have
-                while (count[sortedVowels[vowel_idx]] == 0) {
-                    vowel_idx++;
-                }
-                
-                // Place the vowel and remove one token from its bucket
-                s[i] = sortedVowels[vowel_idx];
-                count[sortedVowels[vowel_idx]]--;
+                s[i] = vowels[v_index++];
             }
         }
-        
+
         return s;
+    }
+
+private:
+    bool isVowel(char c) {
+        return string("AEIOUaeiou").find(c) != string::npos;
     }
 };
