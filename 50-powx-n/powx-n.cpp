0f7fe481b6907e5605d87ad45e426918d677639d
Varun Goyal <142911806+varungoyal03@users.noreply.github.com>
2026-07-08 12:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.28%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-double solvePow(double x, long long exp){
-    if(exp==0) return 1.0;
-
-    if(exp&1){//odd
-       return x*solvePow(x*x,exp/2);
-    }
-
-    return solvePow(x*x,exp/2);
-
-}
     double myPow(double x, int n) {
-     long long exp=n;//imp
+        if(n==0) return 1.0;
 
-     if(exp<0){
-       return solvePow(1.0/x,-exp);
-     }   
+        long long exp = n; // Protect against -2^31 overflow
 
-     return solvePow(x,exp);
+        // Handle negative powers by inverting the base
+        if (exp < 0) {
+            x = 1.0 / x;
+            exp = -exp;
+        }
+
+        double ans = 1.0;
+
+        // If the exponent is odd, multiply the base into our answer
+        while (exp > 0) {
+
+            // IF ODD
+          if (exp & 1) {
+                ans = ans * x;
+                exp = exp - 1; // Subtract 1 to make it even
+            }
+            // IF EVEN
+            else {
+                x = x * x;
+               exp >>= 1; // Cut the power in half
+            }
+        }
+
+        return ans;
     }
 };
